3565d8185cc3ab0606d165103683a1b1823a73de
Luke Howard <lukeh@padl.com>
2023-07-29 14:45:18 +0200

    build Surrogate separately

--- Package.swift
+++ Package.swift
@@ -16,10 +16,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible
         // to other packages.
         .library(
+            name: "Surrogate",
+            targets: [
+                "Surrogate"
+            ]
+        ),
+        .library(
             name: "MOM",
             targets: [
-                "MOM",
-                "Surrogate",
+                "MOM"
             ]
         ),
     ],
